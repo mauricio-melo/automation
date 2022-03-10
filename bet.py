89b9806b0c059f2828823402a0b2341f133fa3a4
ice_mmelo <ice_mmelo@uolinc.com>
2022-03-10 13:21:36 -0300

    Separaçao dos arquivos

--- bet.py
+++ bet.py
@@ -1,27 +1,20 @@
 import pyautogui
 import webbrowser
 import time 
-import sys
-
-url = sys.argv[1]
-isEqual = sys.argv[2]
-isPlus = sys.argv[3]
-isFirstHalf = sys.argv[4]
-isHome = sys.argv[5]
 
 def mouse_position():
-    while True: #Start loop
+    while True: 
         print (pyautogui.position())
         time.sleep(1)
 
-def open_chrome():
+def open_chrome(url):
     webbrowser.get('google-chrome').open(url)
     time.sleep(5)
 
-def login():
+def login(password):
     pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('images/LOGIN.png',confidence=0.7)))
     time.sleep(3)
-    pyautogui.typewrite("F#carioca7")
+    pyautogui.typewrite(password)
     time.sleep(1)
     pyautogui.press("enter")
     time.sleep(6)
@@ -32,7 +25,7 @@ def corner_tab():
     pyautogui.scroll(-8)
     time.sleep(1)      
 
-def select_bet():
+def select_bet(isEqual, isPlus, isFirstHalf, isHome):
     if isEqual.lower() == 'true':
         pyautogui.moveTo(pyautogui.center(pyautogui.locateOnScreen('images/RACE.png',confidence=0.7)))
         if isHome.lower() == 'true':
@@ -51,20 +44,11 @@ def select_bet():
     pyautogui.click()
     time.sleep(1)
 
-def make_bet():
+def make_bet(money):
     pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('images/VALOR_APOSTA.png',confidence=0.7)))
-    pyautogui.typewrite("1")
+    pyautogui.typewrite(money)
     time.sleep(1)
-
     try:
         pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('images/FAZER_APOSTA.png',confidence=0.7)))
     except:
         pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('images/ACEITAR_E_FAZER_APOSTA.png',confidence=0.7)))
-
-
-# mouse_position()
-open_chrome()
-login()
-corner_tab()
-select_bet()
-make_bet()
---
+++ totonero.py
@@ -0,0 +1,14 @@
+import bet 
+import sys
+
+url = sys.argv[1]
+isEqual = sys.argv[2]
+isPlus = sys.argv[3]
+isFirstHalf = sys.argv[4]
+isHome = sys.argv[5]
+
+bet.open_chrome(url)
+bet.login("F#carioca7")
+bet.corner_tab()
+bet.select_bet(isEqual, isPlus, isFirstHalf, isHome)
+bet.make_bet("1")
